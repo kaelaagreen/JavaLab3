a0f0db344cf177061c88e00452b839af69f7cbcc
Kaela <kaelaagreen@gmail.com>
2021-06-16 22:04:06 -0400

    second push

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 "use script"
 
-console.log("hello");
+const submissions = [
+    Jane = {
+        score: 95,
+        date: "2020-01-24",
+        passed: true
+    },
+    Joe = {
+        score: 77,
+        date: "2018-05-14",
+        passed: true
+    },
+    Jack = {
+        score: 59,
+        date: "2019-07-05",
+        passed: false
+    },
+    Jill = {
+        score: 88,
+        date: "2020-04-22",
+        passed: true
+    }];
+
+    addSubmission(array,newName,newScore,newDate)
+
+
+
