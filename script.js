8f71e60bdfad64c81a5e5bfbaa32c330fc03ebc3
Kaela <kaelaagreen@gmail.com>
2021-06-21 18:25:05 -0400

    second push. unfinished.

--- script.js
+++ script.js
@@ -2,27 +2,47 @@
 
 const submissions = [
     Jane = {
+        name: "Jane",
         score: 95,
         date: "2020-01-24",
         passed: true
     },
     Joe = {
+        name: "Joe",
         score: 77,
         date: "2018-05-14",
         passed: true
     },
     Jack = {
+        name: "Jack",
         score: 59,
         date: "2019-07-05",
         passed: false
     },
     Jill = {
+        name: "Jill",
         score: 88,
         date: "2020-04-22",
         passed: true
     }];
 
-    addSubmission(array,newName,newScore,newDate)
+function  addSubmission(array,newName,newScore,newDate){
+    (submissions [array]) = 
+        newName;
+        newScore;
+        newDate;
+
+        if (newScore>60){
+        passed=true;
+        } else {
+        passed=false;
+    }
+// submissions.push(addSubmission(Kaela, Kaela, 90, "2021-06-17")
+let passed = newScore >= 60 ? true : false ; 
+
+// function deleteSubmissionByIndex(array,index){
 
 
+
+// }
 
